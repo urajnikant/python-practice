89ceddd7ec3e092ed498f3d356620938cd1d87e4
urajnikant <unluckyinsaan5@gmail.com>
2025-05-15 22:19:09 +0530

    Added  smallest one.py

---
+++ smallest one.py
@@ -0,0 +1,11 @@
+
+"""Take three numbers and print the smallest one."""
+num1=int(input("enter the num1 "))
+num2=int(input("enter the num2 "))
+num3=int(input("enter the num3 "))
+if num1>num2 or num1>num3:
+    print("num1 is greter ")
+elif(num2>num2 or num2>num3):
+    print("num2 is greter")
+elif(num3>num1 or num3>num2 ):
+    print("num3 is greter ")
